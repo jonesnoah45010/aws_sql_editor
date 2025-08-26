4165724f8b9caa00ce0e10d0513f413c5a4dbc33
Noah Jones <jonesnoah45010@gmail.com>
2025-08-26 13:21:25 -0400

    update

--- app.py
+++ app.py
@@ -247,4 +247,12 @@ def healthz():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)), debug=True)
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+
+
+
+    
